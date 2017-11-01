3351957e16f441d8aad78c3e6f1ecca6585751e7
akecka <agnkecka@gmail.com>
2017-11-01 21:51:16 +0100

    Movies maybe?

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MainMovie.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MainMovie.java
@@ -4,9 +4,11 @@ import java.util.stream.Collectors;
 
 public class MainMovie {
     public static void main(String[] args) {
+
         String listMovies = MovieStore.getMovies().entrySet().stream()
                 .flatMap(MovieStore -> MovieStore.getValue().stream())
                 .collect(Collectors.joining("!"));
+
         System.out.println(listMovies);
     }
 }
