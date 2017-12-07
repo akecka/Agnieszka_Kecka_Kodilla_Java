d230aaaf002f6ab50fc9e0eda6cb2c95edb10643
akecka <agnkecka@gmail.com>
2017-12-07 20:53:09 +0100

    Hibernate_test

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/Task.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/Task.java
@@ -11,6 +11,7 @@ public final class Task {
     private String description;
     private Date created;
     private int duration;
+    private TaskFinancialDetails taskFinancialDetails;
 
     public Task() {
     }
@@ -40,6 +41,11 @@ public final class Task {
     public int getDuration() {
         return duration;
     }
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "TASKS FINANCIALS ID")
+    public TaskFinancialDetails getTaskFinancialDetails() {
+        return taskFinancialDetails;
+    }
 
     private void setId(int id) {
         this.id = id;
@@ -55,5 +61,9 @@ public final class Task {
 
     private void setDuration(int duration) {
         this.duration = duration;
+    }
+
+    public void setTaskFinancialDetails(TaskFinancialDetails taskFinancialDetails) {
+        this.taskFinancialDetails = taskFinancialDetails;
     }
 }
