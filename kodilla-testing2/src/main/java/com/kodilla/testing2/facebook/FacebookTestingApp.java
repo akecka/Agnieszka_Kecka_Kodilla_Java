bf2471ee62b97ad62f90e802a7334242226777a3
akecka <agnkecka@gmail.com>
2018-02-10 13:45:37 +0100

    xpath

--- kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
@@ -1,17 +1,29 @@
 package com.kodilla.testing2.facebook;
 
-
 import com.kodilla.testing2.config.WebDriverConfig;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 public class FacebookTestingApp {
 
     public static void main(String[] args) {
-        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
+        WebDriver driver;
+        WebElement element;
+        Select select;
+
+        driver = WebDriverConfig.getDriver(WebDriverConfig.FIREFOX);
         driver.get("https://www.facebook.com/");
-
-
-
+        element = driver.findElement(By.xpath("//*[@id='day']"));
+        select = new Select(element);
+        select.selectByValue("21");
+        element = driver.findElement(By.xpath("//*[@id='month']"));
+        select = new Select(element);
+        select.selectByValue("1");
+        element = driver.findElement(By.xpath("//*[@id='year']"));
+        select = new Select(element);
+        select.selectByValue("1988");
 
     }
 
